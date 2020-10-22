d0cff5fbb787ffa9065610e5d592c56c42e4b903
Andres <sitosys@gmail.com>
2020-10-22 16:37:07 +0200

    Branch Hello world

---
+++ web/modules/hello_world/src/controller/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Drupal\hello_world\Controller;
+use Drupal\Core\Controller\ControllerBase;
+
+class HelloController extends ControllerBase {
+  public function content() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => $this->t('Hola mundo'),
+    );
+  }
+}
+?>
